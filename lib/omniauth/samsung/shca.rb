159e997d762b3913517d667c383d1564b6e10fe3
Hsiu-Fan Wang <hfwang@porkbuns.net>
2016-03-09 01:08:36 -0800

    Initial commit

---
+++ lib/omniauth/samsung/shca.rb
@@ -0,0 +1,9 @@
+require "omniauth/samsung/shca/version"
+
+module Omniauth
+  module Samsung
+    module Shca
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/omniauth/samsung/shca/version.rb
@@ -0,0 +1,7 @@
+module Omniauth
+  module Samsung
+    module Shca
+      VERSION = "0.1.0"
+    end
+  end
+end
